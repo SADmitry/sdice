c98c966c122a4a2c7fa721f63c4b86aadce37e16
Dmitry <d.a.solovyov@gmail.com>
2024-07-12 15:55:11 +0200

    splitted engine apis (#2)

--- src/main/scala/sdmitry/Engine.scala
+++ src/main/scala/sdmitry/Engine.scala
@@ -5,27 +5,35 @@ import scala.collection.mutable.ArrayBuffer
 
 class DiceEngine(val pool: List[D]):
     /**
+      * Calculate all possible outcomes from given dice pool
+      * @return List of possible outcomes represented by list of dice results
+      */
+    def outcomes(): Outcomes =
+        Outcomes(
+            pool.map {d => d.possibleOutcomes()}
+                .foldRight(ArrayBuffer(ArrayBuffer.empty[Res])) { (currentList, acc) =>
+                    val newAcc = ArrayBuffer[ArrayBuffer[Res]]()
+                    for {
+                        elem <- currentList
+                        rest <- acc
+                    } yield {
+                        newAcc += (elem +: rest)
+                    }
+                    newAcc
+                }
+        )
+
+class Outcomes(val outcomes: Iterable[Iterable[Res]]):
+    /**
     * Calculate statistics from given pool for given type of system
     * @return List of gaming statistics
     */
     def faceToFaceNegating[T <: GamingSystemWithNegation](system: T): List[String] =
-        system.explain(withOutcomes().map ( outcome => system.negation(outcome) ))
+        system.explain(outcomes.map ( outcome => system.negation(outcome) ))
 
-    
-
-    /**
-      * Calculate all possible outcomes from given dice pool
-      * @return List of possible outcomes represented by list of dice results
-      */
-    def withOutcomes(): Iterable[Iterable[Res]] =
-        pool.map {d => d.possibleOutcomes()}
-            .foldRight(ArrayBuffer(ArrayBuffer.empty[Res])) { (currentList, acc) =>
-                val newAcc = ArrayBuffer[ArrayBuffer[Res]]()
-                for {
-                    elem <- currentList
-                    rest <- acc
-                } {
-                  newAcc += (elem +: rest)
-                }
-                newAcc
-            }
+    override def equals(that: Any): Boolean =
+        that match
+            case o: Outcomes =>
+                this.outcomes == o.outcomes
+            case _ =>
+                false
